a575afda7318e7c322534588b0dd881122e0ec53
UNHabitatAndinos <73270842+UNHabitatAndinos@users.noreply.github.com>
2021-07-12 14:04:07 -0500

    finalcom

--- src/map.js
+++ src/map.js
@@ -164,7 +164,7 @@ var legends = {
         elem5: '<div><span  style= "color:#2A115C">▉</span>40 - 59</div>',
         elem6: '',
         elem7: '',
-        elem8: "DANE Censo Nacional Población y Vivienda 2018 - Alcaldía de Barranquilla",
+        elem8: "ONU Hábitat 2019 - Elaboración propia",
     },
 }
 
